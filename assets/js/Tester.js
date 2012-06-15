32136b74f4c277a073a726c309b1431f3f87c6ca
Chris Montrois <chris@chrismontrois.net>
2012-06-15 17:09:06 -0600

    Replacing the ASP page with an HTML one. Used JS to dynamically create boards.

--- assets/js/Tester.js
+++ assets/js/Tester.js
@@ -16,4 +16,34 @@ $(function() {
 	btnPlay.click(function() {
 		gm.start();
 	});
+
+	// Create the tables
+	$("#board-0 tbody").append(createTable(0, "left"));
+	$("#board-1 tbody").append(createTable(1, "right"));
 });
+
+function createTable(index, side) {
+	var tableBody = "";
+
+	for (var i = 0; i < HEIGHT; i++) {
+		var row = "<tr>";
+
+		if (side == "left") {
+			row += "<th>" + i + "</th>";
+		}
+
+		for (var j = 0; j < WIDTH; j++) {
+			row += "<td id='" + index + "-" + j + "," + i + "'></td>";
+		}
+
+		if (side == "right") {
+			row += "<th>" + i + "</th>";
+		}
+
+		row += "</tr>";
+
+		tableBody += row;
+	}
+
+	return tableBody;
+}
